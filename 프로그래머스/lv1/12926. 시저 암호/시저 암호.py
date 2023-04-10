91e1682a811ae4ba1898c289633a9f95ab63b6fb
JoonYoung Yoon <110395625+ZE-R0-1@users.noreply.github.com>
2023-04-10 16:35:27 +0900

    [level 1] Title: 시저 암호, Time: 1.70 ms, Memory: 9.96 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,10 @@
+def solution(s, n):
+    answer = ''
+    for char in s:
+        if char == ' ':
+            answer += ' '
+        elif char.isupper():
+            answer += chr((ord(char) - ord('A') + n) % 26 + ord('A'))
+        else:
+            answer += chr((ord(char) - ord('a') + n) % 26 + ord('a'))
+    return answer
